fc4f3fa0ce4a4cf6a44c88e4a8eb87726d2dee14
Elijah Irizarry <user1@localhost.localdomain>
2017-11-13 21:33:29 -0600

    My js, along with some completed bonus tasks, comments tell rest

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,1 +1,19 @@
-
+$(document).ready(function() {
+  /*if a user has JavaScript, then delete the audio*/
+  $('#ring').remove();
+  /* when the doorbell is pressed, let it ring*/
+  $('#doorbell').on('click', function(){
+    ringDoorbell();
+  });
+  /* ring when the key "r" is pressed */
+  $(document).on("keypress", function(press){
+    if (press.key === "d") {
+      ringDoorbell();
+    }
+  });
+  /*The function that will play the audio*/
+  function ringDoorbell() {
+    var bell = new Audio('media/doorbell.mp3');
+    bell.play();
+  }
+});
